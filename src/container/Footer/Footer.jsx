895f984f5e52d667ec9b1d863e2571ad72f7a392
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-05-05 21:20:43 +0530

    Footer bug fix

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,14 +13,15 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans2">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans2">+1 212-344-1230</p>
+        <p className="p__opensans2">+1 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">
+        {/* <img src={images.gericht} alt="footer_logo" /> */}
+        {/* <h1 className="logo p__opensans2">TasteMate</h1> */}
+        <p className="p__opensans2">
           &quot;The best way to find yourself is to lose yourself in the service
           of others.&quot;
         </p>
@@ -38,15 +39,15 @@ const Footer = () => (
 
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans2">Monday-Friday:</p>
+        <p className="p__opensans2">08:00 am - 12:00 am</p>
+        <p className="p__opensans2">Saturday-Sunday:</p>
+        <p className="p__opensans2">07:00 am - 11:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2023 TasteMate. All Rights reserved.</p>
+      <p className="p__opensans2">2023 TasteMate. All Rights reserved.</p>
     </div>
   </div>
 )
